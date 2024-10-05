cd7f0a5cae0f93a8bc972c8d88be142e5c5efb19
just-abdou <abdelwedoud.lafdal@softwaresavants.dev>
2024-10-05 11:38:55 +0000

    Refactor endpoint URLs in Fetcher class

--- lib/helpers/fetcher.dart
+++ lib/helpers/fetcher.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
@@ -12,7 +11,7 @@ class Fetcher {
 
   Map<String, String> get headers {
     final headers = <String, String>{
-      HttpHeaders.contentTypeHeader: 'application/json',
+      'content-type': 'application/json',
     };
 
     headers['api_key'] = apiKey;
