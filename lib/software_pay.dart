2a98ff12db1c85588b8e37ce0f80eb47eb0a636f
just-abdou <abdelwedoud.lafdal@softwaresavants.dev>
2024-10-07 16:42:26 +0000

    Refactor payment methods export in software_pay.dart

--- lib/software_pay.dart
+++ lib/software_pay.dart
@@ -1,3 +1,3 @@
-export 'src/payment_methods/pages/payment_methods.dart';
+export 'src/payment_methods/pages/home.dart';
 export '/src/payment_methods/models/payment_method_model.dart';
 export 'l10n/generated/software_pay_localization.dart';
